db381511cb03e1d32950069d21161924f5df7314
toasahi <asa1313to@gmail.com>
2021-12-02 17:34:14 +0900

    Eslintの変更.import ReactルールをOFF

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl">こんにちは</h1>
+      <h1 className="text-5xl text-blue-300">こんにちは</h1>
     </div>
   );
 }
